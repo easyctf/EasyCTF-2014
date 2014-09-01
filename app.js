ed28f0a91f592ecaf87da83fe8a0ac8398386dbc
Michael Zhang <failed.down@gmail.com>
2014-08-31 23:35:46 -0500

    Testing POST requests

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/login', routes);
+app.use('/login.ajax', routes);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  res.render('index', { title: 'EasyCTF' });
+	res.render('index', { title: 'EasyCTF' });
 });
 router.get('/login', function(req, res) {
-  res.render('login', { title: 'Login - EasyCTF' });
+	res.render('login', { title: 'Login - EasyCTF' });
+});
+
+router.post('/login.ajax', function(req, res) {
+	res.send('hi');
 });
 
 module.exports = router;

b32e278fee7e16d535b46fe6f83bb16713dd046a
Michael Zhang <failed.down@gmail.com>
2014-08-31 23:17:59 -0500

    Added a login page.

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/login', routes);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'EasyCTF' });
+});
+router.get('/login', function(req, res) {
+  res.render('login', { title: 'Login - EasyCTF' });
 });
 
 module.exports = router;

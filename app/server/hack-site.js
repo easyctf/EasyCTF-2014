e818eeac6811a590ffb7fa94e62649becdeff6d3
Michael Zhang <failed.down@gmail.com>
2014-10-10 18:27:40 -0500

    aaa

--- app/server/hack-site.js
+++ app/server/hack-site.js
@@ -1,5 +1,5 @@
 module.exports = function(app) {
-  app.get("/sites/iti/", function(req, res) {
+  app.get(["/sites/iti", "/sites/iti/", "/sites/iti/index.php"], function(req, res) {
     res.render("hack-site/iti/index", { title: "Institute of Technological Incompetence" });
   });
 };

744e4b5de64bd665e57dcff788b82d503071f38e
Michael Zhang <failed.down@gmail.com>
2014-10-10 20:52:05 -0500

    updated some stuff

--- app/server/hack-site.js
+++ app/server/hack-site.js
@@ -2,4 +2,12 @@ module.exports = function(app) {
   app.get(["/sites/iti", "/sites/iti/", "/sites/iti/index.php"], function(req, res) {
     res.render("hack-site/iti/index", { title: "Institute of Technological Incompetence" });
   });
+
+  app.get("/sites/iti/faculty.php", function(req, res) {
+    res.render("hack-site/iti/faculty", { title: "Faculty - Institute of Technological Incompetence" });
+  });
+
+  app.get("/sites/iti/portal.php", function(req, res) {
+    res.render("hack-site/iti/honor", { title: "Portal - Institute of Technological Incompetence" });
+  });
 };

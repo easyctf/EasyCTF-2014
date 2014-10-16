ab9f132dabddc2994c47a3ded8c5a80f09f0a67d
Michael Zhang <failed.down@gmail.com>
2014-10-15 23:15:43 -0500

    screw you postgresql

--- app/server/hack-site.js
+++ app/server/hack-site.js
@@ -1,6 +1,19 @@
+var pg = require("pg");
+
+var params = {
+  host: 'ec2-54-204-40-96.compute-1.amazonaws.com',
+  user: 'uuauqvjvjlhsjj',
+  password: 'ey2ywt2B5LTxh1Tvsxc2QYKlmT',
+  port: 5432,
+  database: 'd1r3e9shr2fbgn',
+  ssl: true
+};
+
+var connection = "postgres://" + params.user + ":" + params.password + "@" + params.host + ":" + params.port + "/" + params.database + "?ssl=true";
+
 module.exports = function(app) {
   app.get(["/sites/post-it", "/sites/post-it/", "/sites/post-it/index.php"], function(req, res) {
-  	res.render("sites/post-it/index");
+    res.render("sites/post-it/index");
   });
 
   app.post(["/sites/post-it", "/sites/post-it/", "/sites/post-it/index.php"], function(req, res) {
@@ -9,5 +22,55 @@ module.exports = function(app) {
   	} else {
   		res.render("sites/post-it/index");
   	}
+  });
+
+  app.get(["/sites/injection", "/sites/injection/", "/sites/injection/index.php"], function(req, res) {
+    res.render("sites/injection/index");
+  });
+
+  app.post(["/sites/injection", "/sites/injection/", "/sites/injection/index.php"], function(req, res) {
+    var unclean = function(string) {
+      var words = ["select", "insert", "union", "drop", "create", "use", "describe", "table", "from", "where", "count", "now", "distinct", "flush", "into", "destroy"];
+      for(var i=0;i<words.length;i++) {
+        if (string.toLowerCase().indexOf(words[i].toLowerCase()) > -1) {
+          return true;
+        }
+      }
+      return false;
+    };
+
+    if (unclean(req.param("username")) || unclean(req.param("password"))) {
+      res.render("sites/injection/index", {
+        posted: true,
+        error: true
+      });
+    } else {
+      pg.connect(connection, function(err, client, done) {
+        if (err) {
+          console.dir(err);
+          res.render("sites/injection/index", {
+            posted: true,
+            error: true
+          });
+        } else {
+          client.query("SELECT * FROM \"users-a14c001276a69f66fd95104c96c7e4f2\" WHERE username='" + req.param("username") + "' AND password='" + req.param("password") + "'", function(err, result) {
+            if (err) {
+              console.dir(err);
+              res.render("sites/injection/index", {
+                posted: true,
+                error: true
+              });
+            } else {
+              console.dir(result);
+              res.render("sites/injection/index", {
+                posted: true,
+                error: users ? true : false,
+                users: users
+              });
+            }
+          });
+        }
+      });
+    }
   });
 };

3ee051ed634aa65230217cc336b9abd7dd5c66c7
Michael Zhang <failed.down@gmail.com>
2014-10-15 21:01:18 -0500

    whoops forgot to do checking

--- app/server/hack-site.js
+++ app/server/hack-site.js
@@ -1,13 +1,13 @@
 module.exports = function(app) {
-  app.get(["/sites/iti", "/sites/iti/", "/sites/iti/index.php"], function(req, res) {
-    res.render("hack-site/iti/index", { title: "Institute of Technological Incompetence" });
-  });
-
   app.get(["/sites/post-it", "/sites/post-it/", "/sites/post-it/index.php"], function(req, res) {
   	res.render("sites/post-it/index");
   });
 
   app.post(["/sites/post-it", "/sites/post-it/", "/sites/post-it/index.php"], function(req, res) {
-  	res.send("flag: p0st_is_moar_secure_than_g3t");
+  	if (req.param("user") == "flag" && req.param("request") == "flag") {
+	  	res.send("flag: p0st_is_moar_secure_than_g3t");
+  	} else {
+  		res.render("sites/post-it/index");
+  	}
   });
 };

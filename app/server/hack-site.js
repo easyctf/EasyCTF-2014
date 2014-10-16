46389ab68e395f0b0da3efb3ba9c3707f0e2128a
Michael Zhang <failed.down@gmail.com>
2014-10-15 21:02:49 -0500

    user should be admin whoops

--- app/server/hack-site.js
+++ app/server/hack-site.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   });
 
   app.post(["/sites/post-it", "/sites/post-it/", "/sites/post-it/index.php"], function(req, res) {
-  	if (req.param("user") == "flag" && req.param("request") == "flag") {
+  	if (req.param("user") == "admin" && req.param("request") == "flag") {
 	  	res.send("flag: p0st_is_moar_secure_than_g3t");
   	} else {
   		res.render("sites/post-it/index");

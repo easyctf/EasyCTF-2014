628c9694cbba78b2284d403159661afc03247172
Michael Zhang <failed.down@gmail.com>
2014-10-18 15:46:33 -0500

    ok replaced some spaces

--- app/server/hack-site.js
+++ app/server/hack-site.js
@@ -13,12 +13,12 @@ var connection = "postgres://" + params.user + ":" + params.password + "@" + par
 
 module.exports = function(app) {
   app.get(["/sites/cookiezi", "/sites/cookiezi/", "/sites/cookiezi/index.php"], function(req, res) {
-    res.cookie('flag', 'osu_is_love_osu_is_l1fe');
-    res.render("sites/cookiezi/index");
+	res.cookie('flag', 'osu_is_love_osu_is_l1fe');
+	res.render("sites/cookiezi/index");
   });
 
   app.get(["/sites/post-it", "/sites/post-it/", "/sites/post-it/index.php"], function(req, res) {
-    res.render("sites/post-it/index");
+	res.render("sites/post-it/index");
   });
 
   app.post(["/sites/post-it", "/sites/post-it/", "/sites/post-it/index.php"], function(req, res) {
@@ -30,62 +30,62 @@ module.exports = function(app) {
   });
 
   app.get(["/sites/injection", "/sites/injection/", "/sites/injection/index.php"], function(req, res) {
-    res.render("sites/injection/index");
+	res.render("sites/injection/index");
   });
 
   app.post(["/sites/injection", "/sites/injection/", "/sites/injection/index.php"], function(req, res) {
-    var unclean = function(string) {
-      var words = ["select", "insert", "union", "drop", "create", "use", "describe", "table", "from", "where", "count", "now", "distinct", "flush", "into", "destroy"];
-      for(var i=0;i<words.length;i++) {
-        if (string.toLowerCase().indexOf(words[i].toLowerCase()) > -1) {
-          return true;
-        }
-      }
-      return false;
-    };
+	var unclean = function(string) {
+	  var words = ["select", "insert", "union", "drop", "create", "use", "describe", "table", "from", "where", "count", "now", "distinct", "flush", "into", "destroy"];
+	  for(var i=0;i<words.length;i++) {
+		if (string.toLowerCase().indexOf(words[i].toLowerCase()) > -1) {
+		  return true;
+		}
+	  }
+	  return false;
+	};
 
-    if (unclean(req.param("username")) || unclean(req.param("password"))) {
-      res.render("sites/injection/index", {
-        posted: true,
-        error: true
-      });
-    } else {
-      pg.connect(connection, function(err, client, done) {
-        if (err) {
-          res.render("sites/injection/index", {
-            posted: true,
-            error: true
-          });
-        } else {
-          client.query("SELECT * FROM \"users-a14c001276a69f66fd95104c96c7e4f2\" WHERE username='" + req.param("username") + "' AND password='" + req.param("password") + "'", function(err, result) {
-            if (err) {
-              res.render("sites/injection/index", {
-                posted: true,
-                error: true
-              });
-            } else {
-              users = result.rows;
-              res.render("sites/injection/index", {
-                posted: true,
-                error: users ? false : true,
-                users: users
-              });
-            }
-          });
-        }
-      });
-    }
+	if (unclean(req.param("username")) || unclean(req.param("password"))) {
+	  res.render("sites/injection/index", {
+		posted: true,
+		error: true
+	  });
+	} else {
+	  pg.connect(connection, function(err, client, done) {
+		if (err) {
+		  res.render("sites/injection/index", {
+			posted: true,
+			error: true
+		  });
+		} else {
+		  client.query("SELECT * FROM \"users-a14c001276a69f66fd95104c96c7e4f2\" WHERE username='" + req.param("username") + "' AND password='" + req.param("password") + "'", function(err, result) {
+			if (err) {
+			  res.render("sites/injection/index", {
+				posted: true,
+				error: true
+			  });
+			} else {
+			  users = result.rows;
+			  res.render("sites/injection/index", {
+				posted: true,
+				error: users ? false : true,
+				users: users
+			  });
+			}
+		  });
+		}
+	  });
+	}
   });
 
   app.get(["/sites/pointless-keys", "/sites/pointless-keys/", "/sites/pointless-keys/index.php"], function(req, res) {
-    res.render("sites/pointless-keys/index");
+	res.render("sites/pointless-keys/index");
   });
 
   app.post(["/sites/pointless-keys/flag.php"], function(req, res) {
-    var a = req.param("target").join(",");
-    var b = req.param("keys").join(",");
-    if (b.indexOf(a) >= 0) {
-      res.send("flag is konami_c0dez");
-    }
+	var a = req.param("target").join(",");
+	var b = req.param("keys").join(",");
+	if (b.indexOf(a) >= 0) {
+	  res.send("flag is konami_c0dez");
+	}
   });
 };
--- app/server/modules/account-manager.js
+++ app/server/modules/account-manager.js
@@ -5,93 +5,93 @@ var moment = require("moment");
 
 var db = new MongoDB("app29067833", new Server("kahana.mongohq.com", 10071, { auto_reconnect: true }), {w: 1});
 db.open(function(err, db) {
-    if (err) {
-        console.dir(err);
-    } else {
-        console.log("yey connected.");
-        db.authenticate("github_user", "__temporarypassword__", function(err, res) {
-            if (err) {
-                console.dir(err);
-            } else {
-                console.log("yey authenticated");
-            }
-        });
-    }
+	if (err) {
+		console.dir(err);
+	} else {
+		console.log("yey connected.");
+		db.authenticate("github_user", "__temporarypassword__", function(err, res) {
+			if (err) {
+				console.dir(err);
+			} else {
+				console.log("yey authenticated");
+			}
+		});
+	}
 });
 var accounts = db.collection("accounts");
 
 exports.db = db;
 
 exports.autoLogin = function(email, pass, callback) {
-    accounts.findOne({email: email}, function(e, o) {
-        if (o) {
-            o.pass == pass ? callback(o) : callback(null);
-        } else {
-            callback(null);
-        }
-    });
+	accounts.findOne({email: email}, function(e, o) {
+		if (o) {
+			o.pass == pass ? callback(o) : callback(null);
+		} else {
+			callback(null);
+		}
+	});
 };
 
 exports.manualLogin = function(email, pass, callback) {
-    accounts.findOne({email: email}, function(e, o) {
-        if (o == null) {
-            callback("User not found.");
-        } else {
-            validatePassword(pass, o.pass, function(err, res) {
-                if (res) {
-                    callback(null, o);
-                } else {
-                    callback("Invalid password.");
-                }
-            });
-        }
-    });
+	accounts.findOne({email: email}, function(e, o) {
+		if (o == null) {
+			callback("User not found.");
+		} else {
+			validatePassword(pass, o.pass, function(err, res) {
+				if (res) {
+					callback(null, o);
+				} else {
+					callback("Invalid password.");
+				}
+			});
+		}
+	});
 };
 
 exports.addNewAccount = function(newData, callback) {
-    accounts.findOne({teamname: newData.name}, function(e, o) {
-        if (o) {
-            callback("Username is taken.");
-        } else {
-            accounts.findOne({email: newData.email}, function(e, o) {
-                if (o) {
-                    callback("Email is taken.");
-                } else {
-                    saltAndHash(newData.pass, function(hash){
-                        newData.pass = hash;
-                        newData.date = moment().format("MMMM Do YYYY, h:mm:ss a");
-                        newData.group = 1;
-                        newData.pointDisplay = 0;
-                        newData.solved = [];
-                        accounts.insert(newData, { safe: true }, callback);
-                    });
-                }
-            });
-        }
-    });
+	accounts.findOne({teamname: newData.name}, function(e, o) {
+		if (o) {
+			callback("Username is taken.");
+		} else {
+			accounts.findOne({email: newData.email}, function(e, o) {
+				if (o) {
+					callback("Email is taken.");
+				} else {
+					saltAndHash(newData.pass, function(hash){
+						newData.pass = hash;
+						newData.date = moment().format("MMMM Do YYYY, h:mm:ss a");
+						newData.group = 1;
+						newData.pointDisplay = 0;
+						newData.solved = [];
+						accounts.insert(newData, { safe: true }, callback);
+					});
+				}
+			});
+		}
+	});
 };
 
 var generateSalt = function() {
-    var set = "0123456789abcdefghijklmnopqurstuvwxyzABCDEFGHIJKLMNOPQURSTUVWXYZ";
-    var salt = "";
-    for(var i=0; i<16; i++) {
-        var p = Math.floor(Math.random() * set.length);
-        salt += set[p];
-    }
-    return salt;
+	var set = "0123456789abcdefghijklmnopqurstuvwxyzABCDEFGHIJKLMNOPQURSTUVWXYZ";
+	var salt = "";
+	for(var i=0; i<16; i++) {
+		var p = Math.floor(Math.random() * set.length);
+		salt += set[p];
+	}
+	return salt;
 };
 
 var md5 = function(str) {
-    return crypto.createHash('md5').update(str).digest('hex');
+	return crypto.createHash('md5').update(str).digest('hex');
 };
 
 var saltAndHash = function(pass, callback) {
-    var salt = generateSalt();
-    callback(salt + md5(pass + salt));
+	var salt = generateSalt();
+	callback(salt + md5(pass + salt));
 };
 
 var validatePassword = function(plain, hashed, callback) {
-    var salt = hashed.substr(0, 16);
-    var valid = salt + md5(plain + salt);
-    callback(null, hashed === valid);
+	var salt = hashed.substr(0, 16);
+	var valid = salt + md5(plain + salt);
+	callback(null, hashed === valid);
 };

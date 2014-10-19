c6933db183a185e8dfe818a14a529bf2b0d0a9da
Michael Zhang <failed.down@gmail.com>
2014-10-19 11:01:27 -0500

    added output file

--- src/what/what/main.cpp
+++ src/what/what/main.cpp
@@ -8,13 +8,25 @@ int main(int argc, char* argv[]) {
 		cout << "Usage: " << argv[0] << " <filename>" << endl;
 	}
 	else {
-		ifstream file(argv[1]);
-		if (!file.is_open()) {
-			cout << "Could not open the file." << endl;
-		}
-		else {
-			
-		}
+		cout << "Reading from " << argv[1] << "..." << endl;
+
+		ifstream in(argv[1]);
+		char* buf = new char[128];
+
+		in.get(buf[0]);
+		char* header = new char[int(buf[0])];
+
+		in.get(header, int(buf[0]) + 1);
+
+		in.get(buf[0]);
+		char* hash = new char[int(buf[0])];
+
+		in.get(hash, int(buf[0]) + 1);
+		printf("%02X %02X %02X %02X", hash[28], hash[29], hash[30], hash[31]);
+
+		// char* contents = new char[int(buf[0])];
+
+		cout << int(buf[0]);
 	}
 
 	cout << endl;

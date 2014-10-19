b0d2e47f71f7bb522d7c25fcd6a7e44619bc39c3
Michael Zhang <failed.down@gmail.com>
2014-10-19 11:02:44 -0500

    removed stdout thing

--- app/server/hack-site.js
+++ app/server/hack-site.js
@@ -91,6 +91,6 @@ module.exports = function(app) {
   });
 
   app.get(["/sites/what", "/sites/what/", "/sites/what/index.php"], function(req, res) {
-  	res.render("sites/what/index", { text: stdout });
+  	res.render("sites/what/index");
   });
 };

1f5710d14b6daea4737a4eca7ec0e7b0b2617948
Michael Zhang <failed.down@gmail.com>
2014-10-20 18:49:17 -0500

    checks for commandline args

--- src/what/what/what.cpp
+++ src/what/what/what.cpp
@@ -5,8 +5,15 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
 
-	printf("hei, world!\n");
-	printf("%d args found. first arg is %s\n", argc, argv[0]);
+	// printf("hei, world!\n");
+	// printf("%d args found. first arg is %s\n", argc, argv[0]);
+
+	if (argc != 2) {
+		printf("Usage: what <filename>");
+	}
+	else {
+
+	}
 
 	system("pause");
 	return 0;

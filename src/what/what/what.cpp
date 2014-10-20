a07301e57724633dff3c97059ee34808f4d8798a
Michael Zhang <failed.down@gmail.com>
2014-10-20 18:47:31 -0500

    added sample c++ program

---
+++ src/what/what/what.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+	printf("hei, world!\n");
+	printf("%d args found. first arg is %s\n", argc, argv[0]);
+
+	system("pause");
+	return 0;
+
+}

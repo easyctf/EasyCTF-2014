a51876fa73dc51bb4931c726b1fb8f96137d6cd7
Michael Zhang <failed.down@gmail.com>
2014-10-20 18:55:13 -0500

    checks if file exists

--- src/what/what/what.cpp
+++ src/what/what/what.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<stdio.h>
+#include<fstream>
 
 using namespace std;
 
@@ -9,10 +10,19 @@ int main(int argc, char* argv[]) {
 	// printf("%d args found. first arg is %s\n", argc, argv[0]);
 
 	if (argc != 2) {
-		printf("Usage: what <filename>");
+		cout << "Usage: what <filename>" << endl;
 	}
 	else {
+		cout << "Filename provided: " << argv[1] << endl;
 
+		ifstream input(argv[1]);
+
+		if (!input.is_open()) {
+			cout << "File doesn't exist!" << endl;
+		}
+		else {
+			cout << "File exists!" << endl;
+		}
 	}
 
 	system("pause");

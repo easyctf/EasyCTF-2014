77fc2ba61d2f4c2257ef91db4f9582cba70b407b
Michael Zhang <failed.down@gmail.com>
2014-11-09 04:59:53 -0600

    switched from express 4 to express 3

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var stylus = require("stylus");
+var http = require("http");
+
+var app = express();
+
+app.configure(function() {
+	app.set("port", process.env.PORT || 3000);
+    app.set("views", __dirname + "/app/server/views");
+    app.set("view engine", "jade");
+    app.locals.pretty = true;
+
+	app.use(express.bodyParser());
+	app.use(express.cookieParser());
+	app.use(express.session({
+		secret: "lol-so-secret",
+	}));
+    app.use(express.methodOverride());
+	app.use(stylus.middleware({
+		src: __dirname + "/web",
+	}));
+
+	app.use(express.static(__dirname + "/web"));
+});
+
+app.configure("development", function() {
+	app.use(express.errorHandler());
+});
+
+require("./api/api")(app);
+
+http.createServer(app).listen(app.get("port"), function() {
+	console.log("[app.js] listening...");
+});

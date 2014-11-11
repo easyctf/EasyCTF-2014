d5ad835b4575a9b7de84ba6fa6e54c838abac2d2
Michael Zhang <failed.down@gmail.com>
2014-11-10 19:58:35 -0600

    finished login page

--- api/auth.js
+++ api/auth.js
@@ -1,5 +1,7 @@
 var common = require("./common");
 
+var debug_disable_general_login = false;
+
 exports.login = function(req) {
 	if ("tid" in req.session) {
 		return {
@@ -29,10 +31,52 @@ exports.login = function(req) {
 			message: "STAHP!"
 		};
 	}
-	teamCurr = common.db.accounts.find({
+	var teamCurr = common.db.accounts.find({
 		username: teamname
 	});
-	console.dir(teamCurr);
+	
+	if (teamCurr.count() == 0) {
+		return {
+			success: 0,
+			message: "Team " + teamname + " not found."
+		}
+	}
+	if (teamCurr.count() > 1) {
+		// wtf
+		return {
+			success: 0,
+			message: "An error occurred while trying to retrieve your account information."
+		}
+	}
+
+	var checkTeam = teamCurr[0];
+	var pwHash = checkTeam.password;
+
+	if (validatePassword(password, pwHash)) {
+		if (debug_disable_general_login) {
+
+		}
+		if (checkTeam.tid) {
+			req.session.tid = checkTeam.tid;
+		} else {
+			req.session.tid = checkTeam._id.toString();
+			common.db.update({
+				_id: checkTeam._id
+			}, {
+				$set: {
+					tid: checkTeam._id.toString()
+				}
+			});
+		}
+		return {
+			success: 1,
+			message: "Logged in as " + teamname
+		}
+	}
+	return {
+		success: 0,
+		message: "Incorrect password."
+	}
 };
 
 exports.is_logged_in = function(req) {
@@ -47,4 +91,10 @@ exports.is_logged_in = function(req) {
 			message: "You do not appear to be logged in."
 		};
 	}
+};
+
+var validatePassword = function(plain, hashed) {
+	var salt = hashed.substr(0, 16);
+	var valid = salt + md5(plain + salt);
+	return hashed === valid;
 };

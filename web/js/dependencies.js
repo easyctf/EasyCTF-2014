3ecb80e83a3e3a94b8a55115c745b1394afa9cba
Michael Zhang <failed.down@gmail.com>
2014-11-14 21:38:04 -0600

    reload only on success

--- web/js/dependencies.js
+++ web/js/dependencies.js
@@ -132,7 +132,7 @@ function handle_submit(prob_id) {
 		setTimeout(function() {
 			prob_msg.slideUp("normal", function() {
 				prob_msg.html("").show();
-				window.location.reload(true);
+				if (data.status == 1) window.location.reload(true);
 			});
 		}, 2000);
 	});

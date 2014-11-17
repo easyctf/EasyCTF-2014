e6da30ed0ea3e30bc3a866811dab1126991c7b35
Eleng555 <ice_el@sbcglobal.net>
2014-11-16 19:00:35 -0600

    added problem file for QR

---
+++ api/graders/QR.js
@@ -0,0 +1,14 @@
+exports.grade = function(team, key, callback) {
+	var flag = "QRs_r_2D_baRcoDEz";
+	if (typeof key === "string" && key.toLowerCase().indexOf(flag.toLowerCase()) != -1) {
+		callback({
+			correct: true,
+			message: "Nice job!"
+		});
+	} else {
+		callback({
+			correct: false,
+			message: "Hm... try again."
+		});
+	}
+};

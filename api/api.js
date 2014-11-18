3c5a0d48c56ab61495972952d1c2876b38276cc3
Michael Zhang <failed.down@gmail.com>
2014-11-18 17:28:25 -0500

    added stuff

--- api/api.js
+++ api/api.js
@@ -58,7 +58,7 @@ module.exports = function(app) {
 	});
 
 	app.get("/api/problems", function(req, res) {
-		if (auth.is_logged_in(req) && auth.is_authorized(req).status === 1) {
+		if (auth.is_logged_in(req) && auth.is_authorized(req).success === 1) {
 			problem.load_unlocked_problems(req.session.tid, function(unlocked) {
 				res.send(unlocked);
 			});

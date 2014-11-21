e7999b8f20a3e174e8ff4567391591fe6ae83991
Michael Zhang <failed.down@gmail.com>
2014-11-21 03:02:23 -0600

    modified python basic 4

--- api/ide/combination.js
+++ api/ide/combination.js
@@ -1,13 +1,35 @@
 var flag = "putting_it_all_t0gether";
 
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
 exports.get_data = function(req, callback) {
 	var result = [];
 
 	result.push("Let's put all your skills together now. ");
 	result.push('A');
 	result.push("thisisastringoflength23");
-	result.push(1787569);
+	result.push(Math.pow(Math.floor(Math.random()*12409)+182740, 2));
 	result.push("wouldn't it be great if this were a palindrome");
+
+	result = shuffle(result);
+
 	req.session.data = result;
 	callback(result);
 };
@@ -16,9 +38,9 @@ exports.check_data = function(req, callback) {
 	var data = req.session.data;
 	var ans = "";
 	var a = data;
-	var b = typeof a[2];
+	var b = typeof a[1];
 	function reverse(s) {
-	return s.split('').reverse().join('');
+		return s.split('').reverse().join('');
 	}
 	ans = a[0]+String(b)+String(a[2].length)+String(Math.sqrt(a[3]))+String(reverse(a[4]));
 	var answer = req.param("answer");

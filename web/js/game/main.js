b55d4300a67fa7bcf5b4ab762f8edb3a30c491ba
Michael Zhang <failed.down@gmail.com>
2014-11-23 02:35:33 -0600

    added a (somewhat) framework

--- web/js/game/main.js
+++ web/js/game/main.js
@@ -4,6 +4,7 @@ var oldTime;
 
 $.when(
 	$.getScript(jsdir + "loader.js"),
+	$.getScript(jsdir + "save.js"),
 	$.getScript(jsdir + "canvas.js"),
 	$.getScript(jsdir + "screen.js"),
 	$.getScript(jsdir + "run.js")

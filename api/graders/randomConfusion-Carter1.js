513e82e88eb015fa7199fe0a4244912489f28f81
Michael Zhang <failed.down@gmail.com>
2014-11-24 21:38:42 -0600

    forgot what this does

--- api/graders/randomConfusion-Carter1.js
+++ api/graders/randomConfusion-Carter1.js
@@ -1,3 +1,20 @@
+exports.grade = function(team, key, callback) {
+	var flag = "QRs_r_2D_baRcoDEz";
+	if (typeof key === "string" && key.toLowerCase().indexOf(flag.toLowerCase()) != -1) {
+		callback({
+			correct: true,
+			message: "Nice job!"
+		});
+	} else {
+		callback({
+			correct: false,
+			message: "Hm... try again."
+		});
+	}
+};
+
+// 794477.627260174
+
 function checkRandomConfusion(doubleVal)
 {
 	if(typeof doubleVal === 'number'&&doubleVal!==0)

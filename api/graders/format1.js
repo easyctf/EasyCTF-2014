05045673a8a0b358824557b2d810c86cb9bb03b4
Eleng555 <ice_el@sbcglobal.net>
2014-11-28 17:07:03 -0600

    changed flag for format1

--- api/graders/format1.js
+++ api/graders/format1.js
@@ -1,5 +1,5 @@
 exports.grade = function(team, key, callback) {
-	if (typeof key === "string" && key.toLowerCase().indexOf("this_is_an_uncreative_flag".toLowerCase()) != -1) {
+	if (typeof key === "string" && key.toLowerCase().indexOf("it's over 9000!!11!1one1!".toLowerCase()) != -1) {
 		callback({
 			correct: true,
 			message: "Awesome!"

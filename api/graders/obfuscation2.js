23c2a3fc346d64af169eea6a1e1cdcf0c596fbb7
Michael Zhang <failed.down@gmail.com>
2014-11-27 19:31:18 -0600

    fixed obfuscate2.js

--- api/graders/obfuscation2.js
+++ api/graders/obfuscation2.js
@@ -1,5 +1,5 @@
 exports.grade = function(team, key, callback) {
-	if (typeof key === "string" && key.indexOf("0bfuscaTion fTw") != -1) {
+	if (typeof key === "string" && key.toLowerCase().indexOf("0bfuscaTion fTw".toLowerCase()) != -1) {
 		callback({
 			correct: true,
 			message: "Nice job!"

439e6c1f4e7fc9888806ba61abbad4b72e1f50e1
Michael Zhang <failed.down@gmail.com>
2014-11-27 23:02:11 -0500

    added format1 files

--- problems/format1/format1.c
+++ problems/format1/format1.c
@@ -1,19 +1,22 @@
-#include <stdio.h>                                                                             
-#include <stdlib.h>                                                                            
-#include <fcntl.h>                                                                             
-                                                                                            
-int key = 0;                                                                                              
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
 
-void give_shell(){                                                                             
-    gid_t gid = getegid();                                                                     
-    setresgid(gid, gid, gid);                                                                  
-    system("/bin/sh -i");                                                                      
-}                                                                                             
+int key = 0;
 
-int main(int argc, char **argv){                                                               
-    int *ptr = &key;                                                                        
-    printf(argv[1]);                                                                                           
-    if (key > 9000){                                                                       
-        give_shell();                                                                          
-    }                                                                                          
-    return 0;                                                                                  
+void vuln(){
+    gid_t gid = getegid();
+    setresgid(gid, gid, gid);
+    system("/bin/sh -i");
+}
+
+int main(int argc, char **argv){
+    int *ptr = &key;
+    printf(argv[1]);
+
+    if (key > 9000){
+        vuln();
+    }
+    return 0;
+}
+

d447629988a4fe86848e08cee8dfd8c40ede6772
Eleng555 <ice_el@sbcglobal.net>
2014-11-29 01:25:58 -0600

    fixed link to A000045, updated file for format2

--- web/problem_data/format2/format2.c
+++ web/problem_data/format2/format2.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-
-int main(int argc, char **argv){
-    printf(argv[1]);
-    gid_t gid = getegid();
-    setresgid(gid, gid, gid);
-    system("/bin/pwd");
-    return 0;
-}
+#include <stdio.h>                                                               
+#include <stdlib.h>                                                              
+#include <fcntl.h>                                                               
+                                                                                 
+int main(int argc, char **argv){                                                 
+    char* cmd = "/bin/pwd";                                                      
+    printf(argv[1]);                                                             
+    gid_t gid = getegid();                                                       
+    setresgid(gid, gid, gid);                                                    
+    system(cmd);                                                                 
+    return 0;                                                                    
+}      

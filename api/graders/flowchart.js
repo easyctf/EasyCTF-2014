e0c312c99b34cd98b2b31e29c35136f79685a32a
Eleng555 <ice_el@sbcglobal.net>
2014-11-29 00:55:50 -0600

    fixed grader for flowchart

--- api/graders/flowchart.js
+++ api/graders/flowchart.js
@@ -1,5 +1,5 @@
 exports.grade = function(team, key, callback) {
-	if (typeof key === "string" && key.toLowerCase().indexOf("02402402401234073250123402020202020297499161411161614111616131818") != -1) {
+	if (typeof key === "string" && key.toLowerCase().indexOf("0240240240123407325012340202020297499161411161614111616131818") != -1) {
 		callback({
 			correct: true,
 			message: "Nice job!"

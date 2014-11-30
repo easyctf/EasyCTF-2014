4346eacf9676701c845c9841becb5a4d5722a003
Michael Zhang <failed.down@gmail.com>
2014-11-30 17:18:50 -0600

    updated mild guess

--- web/problem_data/mild_guess/guess.py
+++ web/problem_data/mild_guess/guess.py
@@ -3,33 +3,37 @@ import time
 import socket
 import thread
 
-host = ''
+host = ""
 port = 10661
 backlog = 5
 size = 1024
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host,port))
 s.listen(backlog)
-flag="xxxxxx"
+flag="NOT_HERE"
 def clientthread(conn):
+    conn.sendall("%s\n" % "try to guess my secret!");
     random.seed()
     random_secret=random.random()
     print "Just type in: ", random_secret
     data = conn.recv(1024)
-    print data,float(data),type(data),random_secret,type(random_secret)
+    # print data,float(data),type(data),random_secret,type(random_secret)
+    try:
+        float(data);
+    except Exception:
+        conn.close();
+        return;
     if float(data)==random_secret:
         conn.sendall(flag)
     else:
         conn.sendall("Nope!")
-    conn.close()
- 
+    conn.close();
+
 #now keep talking with the client
 while 1:
     #wait to accept a connection - blocking call
     conn, addr = s.accept()
-    print 'Connected with ' + addr[0] + ':' + str(addr[1])
-     
-    #start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
+    # print 'Connected with ' + addr[0] + ':' + str(addr[1])
+
+    #start new thread takes 1st argument as a function name to be run, second is     #the tuple of arguments to the function.
     thread.start_new_thread(clientthread ,(conn,))
- 
-s.close()

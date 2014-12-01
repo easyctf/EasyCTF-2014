fb56292aa7c478fad4c77bf36fe9c3f0cf1b7ff0
Michael Zhang <failed.down@gmail.com>
2014-12-01 16:33:30 -0600

    updated mild guess

--- web/problem_data/mild_guess/guess.py
+++ web/problem_data/mild_guess/guess.py
@@ -1,39 +1,39 @@
-import random
-import time
-import socket
-import thread
+import random                                                                                                                                                                                 
+import time                                                                                                                                                                                   
+import socket                                                                                                                                                                                 
+import thread                                                                                                                                                                                 
+                                                                                                                                                                                              
+host = ""                                                                                                                                                                                     
+port = 10661                                                                                                                                                                                  
+backlog = 5                                                                                                                                                                                   
+size = 1024                                                                                                                                                                                   
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                                                                                                                                         
+s.bind((host,port))                                                                                                                                                                           
+s.listen(backlog)                                                                                                                                                                             
+flag="xxxxxxxx"                                                                                                                                                                   
+def clientthread(conn):                                                                                                                                                                       
+    conn.sendall("%s\n" % "try to guess my secret!");                                                                                                                                         
+    random.seed()                                                                                                                                                                             
+    random_secret=random.random()                                                                                                                                                             
+    print "Just type in: ", random_secret                                                                                                                                                     
+    data = conn.recv(1024)                                                                                                                                                                    
+    # print data,float(data),type(data),random_secret,type(random_secret)                                                                                                                     
+    try:                                                                                                                                                                                      
+        float(data);                                                                                                                                                                          
+    except Exception:                                                                                                                                                                         
+        conn.close();                                                                                                                                                                         
+        return;                                                                                                                                                                               
+    if abs(float(data)-random_secret) < 0.00000001:                                                                                                                                           
+        conn.sendall(flag)                                                                                                                                                                    
+    else:                                                                                                                                                                                     
+        conn.sendall("Nope!")                                                                                                                                                                 
+    conn.close();                                                                                                                                                                             
 
-host = ""
-port = 10661
-backlog = 5
-size = 1024
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((host,port))
-s.listen(backlog)
-flag="NOT_HERE"
-def clientthread(conn):
-    conn.sendall("%s\n" % "try to guess my secret!");
-    random.seed()
-    random_secret=random.random()
-    print "Just type in: ", random_secret
-    data = conn.recv(1024)
-    # print data,float(data),type(data),random_secret,type(random_secret)
-    try:
-        float(data);
-    except Exception:
-        conn.close();
-        return;
-    if float(data)-random_secret<10**-8:
-        conn.sendall(flag)
-    else:
-        conn.sendall("Nope!")
-    conn.close();
+#now keep talking with the client                                                                                                                                                             
+while 1:                                                                                                                                                                                      
+    #wait to accept a connection - blocking call                                                                                                                                              
+    conn, addr = s.accept()                                                                                                                                                                   
+    # print 'Connected with ' + addr[0] + ':' + str(addr[1])                                                                                                                                  
 
-#now keep talking with the client
-while 1:
-    #wait to accept a connection - blocking call
-    conn, addr = s.accept()
-    # print 'Connected with ' + addr[0] + ':' + str(addr[1])
-
-    #start new thread takes 1st argument as a function name to be run, second is     #the tuple of arguments to the function.
-    thread.start_new_thread(clientthread ,(conn,))
+    #start new thread takes 1st argument as a function name to be run, second is     #the tuple of arguments to the function.                                                                 
+    thread.start_new_thread(clientthread ,(conn,)) 

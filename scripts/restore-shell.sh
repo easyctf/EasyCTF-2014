a2930861f98d1ad92883016662bafa80f8966057
Michael Zhang <failed.down@gmail.com>
2014-12-04 01:47:10 -0500

    stuff

--- scripts/restore-shell.sh
+++ scripts/restore-shell.sh
@@ -1,2 +1,2 @@
 # mongo --host kahana.mongohq.com --port 10071 -u github_user -p __temporarypassword__ --verbose --eval "db.shell_accounts.remove({})" app29067833
-mongoimport --host kahana.mongohq.com --port 10071 --db app29067833 -u github_user -p __temporarypassword__ --verbose --file accounts.csv -c shell_accounts --type csv --headerline
+mongoimport --host kahana.mongohq.com --port 10071 --db app29067833 -u github_user -p __temporarypassword__ --verbose --file moar_accts.csv -c shell_accounts --type csv --headerline

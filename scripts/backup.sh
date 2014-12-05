717d92e77b94873796b5a61904f02a64e9caa2e2
Michael Zhang <failed.down@gmail.com>
2014-12-05 00:09:57 -0600

    added hint 3

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,6 +1,6 @@
-mongo --host kahana.mongohq.com --port 10071 -u github_user -p __temporarypassword__ --verbose --eval "db.submissions.remove({})" app29067833
-mongo --host kahana.mongohq.com --port 10071 -u github_user -p __temporarypassword__ --verbose --eval "db.reset_password.remove({})" app29067833
-mongoexport --host kahana.mongohq.com --port 10071 --db app29067833 -u github_user -p __temporarypassword__ --verbose -o final/accounts.json -c accounts
-mongoexport --host kahana.mongohq.com --port 10071 --db app29067833 -u github_user -p __temporarypassword__ --verbose -o final/problems.json -c problems
-mongoexport --host kahana.mongohq.com --port 10071 --db app29067833 -u github_user -p __temporarypassword__ --verbose -o final/shell_accounts.json -c shell_accounts
-mongoexport --host kahana.mongohq.com --port 10071 --db app29067833 -u github_user -p __temporarypassword__ --verbose -o final/problems.json -c problems
+# mongo --host kahana.mongohq.com --port 10071 -u github_user -p __temporarypassword__ --verbose --eval "db.submissions.remove({})" app29067833
+# mongo --host kahana.mongohq.com --port 10071 -u github_user -p __temporarypassword__ --verbose --eval "db.reset_password.remove({})" app29067833
+mongoexport --host kahana.mongohq.com --port 10071 --db app29067833 -u github_user -p __temporarypassword__ --verbose -o during/accounts.json -c accounts
+mongoexport --host kahana.mongohq.com --port 10071 --db app29067833 -u github_user -p __temporarypassword__ --verbose -o during/problems.json -c problems
+mongoexport --host kahana.mongohq.com --port 10071 --db app29067833 -u github_user -p __temporarypassword__ --verbose -o during/shell_accounts.json -c shell_accounts
+mongoexport --host kahana.mongohq.com --port 10071 --db app29067833 -u github_user -p __temporarypassword__ --verbose -o during/problems.json -c problems

18cac40720359301c2cea9ea799ac09ad6a7d1dd
Michael Zhang <failed.down@gmail.com>
2014-12-05 00:09:57 -0600

    added hint 3

---
+++ scripts/export-accounts.sh
@@ -0,0 +1,3 @@
+# mongo --host kahana.mongohq.com --port 10071 -u github_user -p __temporarypassword__ --verbose --eval "db.submissions.remove({})" app29067833
+# mongo --host kahana.mongohq.com --port 10071 -u github_user -p __temporarypassword__ --verbose --eval "db.reset_password.remove({})" app29067833
+mongoexport --host kahana.mongohq.com --port 10071 --db app29067833 -u github_user -p __temporarypassword__ --verbose -o during/accounts.csv -c accounts --csv --fields _id,email
